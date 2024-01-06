203a0692a03531fcaed0ba5c77da33146ee6bda3
Jarvis Rwabuyongo <jarvisrwabuyongo45@gmail.com>
2024-01-07 00:06:22 +0300

    Fix code

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    if idx < 1:
+    if idx < 0:
         return None
-    elif idx > len(mylist):
+    elif idx > (len(mylist) - 1):
         return None
     else:
         for item in my_list:
