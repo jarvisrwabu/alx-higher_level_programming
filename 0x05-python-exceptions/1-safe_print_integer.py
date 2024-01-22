9c4443adfc955bebb3fb86af1b0a31730d824c90
Jarvis Rwabuyongo <jarvisrwabuyongo45@gmail.com>
2024-01-22 22:04:47 +0300

    Update 1-safe_print_integer.py - Add TypeError

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
+# Add TypeError
 def safe_print_integer(value):
     try:
         print("{:d}".format(value))
-    except ValueError:
+    except (ValueError, TypeError):
         return False
     return True
