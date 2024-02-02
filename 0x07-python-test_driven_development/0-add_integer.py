4c2705d1fcda031c369c6f4976af0919f4121ec5
jarvisrwabuyongo <jarvisrwabuyongo45@gmail.com>
2024-02-03 01:28:23 +0300

    Add two integers

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+def add_integer(a, b=98):
+    """Adds two integers.
+    
+    Args:
+        a (int)(float): An integer or float
+        b (int)(float): An integer or float
+    """
+    if not (isinstance(a, (int, float)) and isinstance(b, (int, float))):
+        raise TypeError("a must be an integer or b must be an integer")
+    
+    elif isinstance(a, float) or isinstance(b, float):
+        a = int(a)
+        b = int(b)
+        return (a+b)
+    
+    else:
+        return (a+b)
