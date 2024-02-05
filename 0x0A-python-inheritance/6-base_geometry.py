dc62b953875dfb6093fa72f7ace9eabf50e59f9c
jarvisrwabuyongo <jarvisrwabuyongo45@gmail.com>
2024-02-06 01:38:23 +0300

    Add method to BaseClass

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Create a new BaseClass."""
+
+
+class BaseGeometry:
+    """BaseClass to be inherited."""
+
+    def area(self):
+        raise Exception("area() is not implemented")
