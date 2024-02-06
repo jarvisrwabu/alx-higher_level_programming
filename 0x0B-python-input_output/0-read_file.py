3e9d4eabaac00047d0df0abca2450fcfffd84af0
jarvisrwabuyongo <jarvisrwabuyongo45@gmail.com>
2024-02-06 17:23:20 +0300

    Fix code syntax and styling

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -3,6 +3,7 @@
 
 
 def read_file(filename=""):
-    with open("filename", "r") as file:
+    """Read a file."""
+    with open(filename, mode='r', encoding='UTF8') as file:
         content = file.read()
         print(content)
