52905cdfeaae7f7d19fd4bb6f025b63fed7ee18c
jarvisrwabuyongo <jarvisrwabuyongo45@gmail.com>
2024-02-08 00:18:39 +0300

    BaseGeometry Class does not take parameters(Is not initialized)

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -5,11 +5,6 @@
 class BaseGeometry:
     """BaseClass to be inherited."""
 
-    def __init__(self, name, value):
-        """Initialize an object."""
-        self.name = name
-        self.value = value
-
     def integer_validator(self, name, value):
         """Validate integer."""
         if not isinstance(value, int):
