1b0c8194e88819638509c90049ed68a0c3c0821c
jarvisrwabuyongo <jarvisrwabuyongo45@gmail.com>
2024-02-07 16:13:02 +0300

    Student to JSON

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Get dictionary description within a class."""
+
+
+class Student:
+    """Blueprint of student."""
+
+    def __init__(self, first_name, last_name, age):
+        """When initializing new object."""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """Get dictionary representation of student instance."""
+        self.__dict__
