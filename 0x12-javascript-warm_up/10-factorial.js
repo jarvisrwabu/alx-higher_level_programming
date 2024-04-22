12d841e13da0040eec4196d3eaef05437ae7f977
jarvisrwabu <jarvisrwabuyongo45@gmail.com>
2024-04-22 20:36:31 +0300

    Use isNaN to check iincoming parameter

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -3,7 +3,7 @@
 const { argv } = require('node:process');
 
 function factorial (num) {
-  if (num === 1 || num === 0) {
+  if (num === 1 || num === 0 || isNaN(num)) {
     return 1;
   } else {
     return num * factorial(num - 1);
