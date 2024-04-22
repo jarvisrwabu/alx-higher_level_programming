d04c3cb49b734788c1103970bfddeac6b8f41fff
jarvisrwabu <jarvisrwabuyongo45@gmail.com>
2024-04-22 17:27:55 +0300

    Calculate factorial recursively

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+const { argv } = require('node:process');
+
+function factorial (num) {
+  if (num === 1 || num === 0) {
+    return 1;
+  } else {
+    return num * factorial(num - 1);
+  }
+}
+
+console.log(factorial(argv[2]));
