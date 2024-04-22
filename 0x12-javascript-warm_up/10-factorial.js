5c53f80c7cbb450a4025e0cf2bd09d735a967dba
jarvisrwabu <jarvisrwabuyongo45@gmail.com>
2024-04-22 17:32:43 +0300

    Add logic to check NaN

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -3,7 +3,7 @@
 const { argv } = require('node:process');
 
 function factorial (num) {
-  if (num === 1 || num === 0) {
+  if (num === 1 || num === 0 || num === NaN) {
     return 1;
   } else {
     return num * factorial(num - 1);
