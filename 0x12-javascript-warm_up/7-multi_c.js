20d39a91937c5925ecc186f15a020e92f0e895af
jarvisrwabu <jarvisrwabuyongo45@gmail.com>
2024-04-22 17:06:34 +0300

    Print string x times

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const { argv } = require('node:process');
+
+if (Number(argv[2])) {
+  for (let i = 0; i < Number(argv[2]); i++) {
+    console.log('C is fun');
+  }
+} else {
+  console.log('Missing number of occurrences');
+}
