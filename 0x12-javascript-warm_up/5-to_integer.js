845c1e12b2170ed7ba58dde7ec5d3885cd6c9307
jarvisrwabu <jarvisrwabuyongo45@gmail.com>
2024-04-23 10:25:07 +0300

    Fix space error

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -3,7 +3,7 @@
 const { argv } = require('node:process');
 
 if (Number(argv[2])) {
-  console.log('My number : ' + Number(argv[2]));
+  console.log('My number: ' + Number(argv[2]));
 } else {
   console.log('Not a number');
 }
