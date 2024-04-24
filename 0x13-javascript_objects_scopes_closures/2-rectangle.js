0ca6d82725a3ed983219c5791dd67b51abd3b9a4
jarvisrwabu <jarvisrwabuyongo45@gmail.com>
2024-04-24 15:36:26 +0300

    Modify class Rectangle

---
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+module.exports = class Rectangle {
+  constructor (w, h) {
+    this.width = w;
+    this.height = h;
+
+    if (self.width <= 0 || self.height <= 0 ) {
+      const Rectangle = {};
+    }
+  }
+}
