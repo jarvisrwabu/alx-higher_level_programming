2cdec17d9f85f340a946fc8a938f3a7d1a0d6b1f
jarvisrwabu <jarvisrwabuyongo45@gmail.com>
2024-04-24 18:55:27 +0300

    Fix semistandard errors

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -2,7 +2,8 @@
 module.exports = class Rectangle {
   constructor (w, h) {
     if (w <= 0 || h <= 0 || w === undefined || h === undefined) {
-      return {};
+      const Rectangle = {};
+      return 'Rectangle' + Rectangle;
     } else {
       this.width = w;
       this.height = h;
