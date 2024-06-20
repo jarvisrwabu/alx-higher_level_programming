81e1abb8be56825fc629a579c08e8286ca54dc0b
jarvisrwabu <jarvisrwabuyongo45@gmail.com>
2024-06-20 14:40:10 +0300

    Fix Semistandard

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -1,31 +1,30 @@
 #!/usr/bin/node
 module.exports = class Rectangle {
-    constructor (w, h) {
-      if (w <= 0 || h <= 0 || w === undefined || h === undefined) {
-        const Rectangle = {};
-        return 'Rectangle' + Rectangle;
-      } else {
-        this.width = w;
-        this.height = h;
-      }
+  constructor (w, h) {
+    if (w <= 0 || h <= 0 || w === undefined || h === undefined) {
+      const Rectangle = {};
+      return 'Rectangle' + Rectangle;
+    } else {
+      this.width = w;
+      this.height = h;
     }
-  
-    print () {
-      for (let i = 0; i < this.height; i++) {
-        for (let j = 0; j < this.width; j++) {
-          process.stdout.write('X');
-        }
-        console.log();
-      }
-    }
+  }
 
-    rotate () {
-      [this.width, this.height] = [this.height, this.width];
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      for (let j = 0; j < this.width; j++) {
+        process.stdout.write('X');
+      }
+      console.log();
     }
+  }
 
-    double () {
-      this.width *= 2;
-      this.height *= 2;
-    }
-  };
-  
+  rotate () {
+    [this.width, this.height] = [this.height, this.width];
+  }
+
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
+};
