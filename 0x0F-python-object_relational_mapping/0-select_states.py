ed3b63e063fc6f2bd3e7e2b2049b56ead7a15c72
jarvisrwabu <jarvisrwabuyongo45@gmail.com>
2024-06-21 16:13:33 +0300

    Fic pycodestyle

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -2,6 +2,12 @@
 """Connect to a MySQL server using the module MySQLdb."""
 
 import MySQLdb
-db = MySQLdb.connect(host="localhost", port=3306, user="root", passwd="root", db="hbtn_0e_0_usa")
+db = MySQLdb.connect(
+    host="localhost",
+    port=3306,
+    user="root",
+    passwd="root",
+    db="hbtn_0e_0_usa"
+)
 cursor = db.cursor()
 cursor.execute("SELECT * FROM states ORDER BY id")
