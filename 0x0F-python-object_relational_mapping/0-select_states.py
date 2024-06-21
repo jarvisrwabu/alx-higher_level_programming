e26ed0c07297873f8b2a73b4d7ffd0b1c765ab0b
jarvisrwabu <jarvisrwabuyongo45@gmail.com>
2024-06-21 15:55:08 +0300

    Fix requirement errors

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -3,4 +3,4 @@
 import MySQLdb
 db = MySQLdb.connect(host="localhost", port=3306, user="root", passwd="root", db="hbtn_0e_0_usa")
 cursor = db.cursor()
-cursor.execute("SELECT * FROM states ORDER BY id ASC")
+cursor.execute("SELECT * FROM states ORDER BY id")
