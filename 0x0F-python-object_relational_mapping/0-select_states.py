cf36ca3ff70e557624c5d0ec1f3e91cfd7ecbbfd
jarvisrwabu <jarvisrwabuyongo45@gmail.com>
2024-06-22 13:52:31 +0300

    Add boilerplate code

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -3,18 +3,21 @@
 
 import MySQLdb
 import sys
-db = MySQLdb.connect(
-    host="localhost",
-    port=3306,
-    user=sys.argv[1],
-    passwd=sys.argv[2],
-    db=sys.argv[3]
-)
-cursor = db.cursor()
-cursor.execute("SELECT * FROM states ORDER BY id")
-rows = cursor.fetchall()
-for row in rows:
-    print(row)
 
-cursor.close()
-db.close()
+if __name__ == "__main__":
+    
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3]
+    )
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states ORDER BY id")
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
+
+    cursor.close()
+    db.close()
