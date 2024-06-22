0e3a67c2e53a466e4f91fdb40b07af87038382dd
jarvisrwabu <jarvisrwabuyongo45@gmail.com>
2024-06-22 13:34:04 +0300

    Get input from command line arguments

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -2,12 +2,13 @@
 """Connect to a MySQL server using the module MySQLdb."""
 
 import MySQLdb
+import sys
 db = MySQLdb.connect(
     host="localhost",
     port=3306,
-    user="root",
-    passwd="root",
-    db="hbtn_0e_0_usa"
+    user=sys.argv[1],
+    passwd=sys.argv[2],
+    db=sys.argv[3]
 )
 cursor = db.cursor()
 cursor.execute("SELECT * FROM states ORDER BY id")
