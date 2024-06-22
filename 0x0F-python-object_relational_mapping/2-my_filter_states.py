fa13fe463d27eb174a172af23375d6e0b8a928b6
jarvisrwabu <jarvisrwabuyongo45@gmail.com>
2024-06-22 15:16:49 +0300

    Return state from query

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+"""Return state from argument."""
+
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+
+    # connect to database
+    db = MySQLdb.connect(host="localhost",
+                         port=3306,
+                         user=argv[1],
+                         passwd=argv[2],
+                         db=argv[3])
+
+    # create cursor to exec queries using SQL; filter names starting with 'N'
+    cursor = db.cursor()
+    query = "SELECT * FROM states WHERE name LIKE %s "
+    cursor.execute(query, (argv[4], ))
+    for row in cursor.fetchall():
+        print(row)
+    cursor.close()
+    db.close()
