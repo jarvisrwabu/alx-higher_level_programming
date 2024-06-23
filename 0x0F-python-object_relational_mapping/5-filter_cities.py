10ce10f5413574642bd0704fd8d4eb3a03c8fb59
jarvisrwabu <jarvisrwabuyongo45@gmail.com>
2024-06-23 12:24:06 +0300

    Change Logic for placeholder

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -19,7 +19,7 @@ if __name__ == "__main__":
     query = """SELECT cities.id, cities.name, states.name
                FROM cities
                LEFT JOIN states ON cities.state_id = states.id
-               WHERE states.name = %s
+               WHERE states.name LIKE %s
                ORDER BY cities.id ASC"""
     cursor.execute(query, (argv[4],))
     for row in cursor.fetchall():
