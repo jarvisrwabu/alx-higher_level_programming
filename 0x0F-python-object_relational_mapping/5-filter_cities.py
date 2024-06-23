9d201578b15b1fa41fbb5caf1d3f3b2fa16fd6f7
jarvisrwabu <jarvisrwabuyongo45@gmail.com>
2024-06-23 12:21:39 +0300

    Filter cities

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+"""Filter cities from a database."""
+
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+
+    # connect to database
+    db = MySQLdb.connect(host="localhost",
+                         port=3306,
+                         user=argv[1],
+                         passwd=argv[2],
+                         db=argv[3])
+
+    # create cursor to exec queries using SQL; list all cities by state
+    cursor = db.cursor()
+    query = """SELECT cities.id, cities.name, states.name
+               FROM cities
+               LEFT JOIN states ON cities.state_id = states.id
+               WHERE states.name = %s
+               ORDER BY cities.id ASC"""
+    cursor.execute(query, (argv[4],))
+    for row in cursor.fetchall():
+        print(row)
+    cursor.close()
+    db.close()
