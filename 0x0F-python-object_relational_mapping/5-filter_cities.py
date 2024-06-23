65b80c012a285cfa96d1ebe04a4235fa0b0f77fb
jarvisrwabu <jarvisrwabuyongo45@gmail.com>
2024-06-23 12:28:47 +0300

    Change query logic

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -18,7 +18,7 @@ if __name__ == "__main__":
     cursor = db.cursor()
     query = """SELECT cities.id, cities.name, states.name
                FROM cities
-               LEFT JOIN states ON cities.state_id = states.id
+               INNER JOIN states ON cities.state_id = states.id
                WHERE states.name LIKE %s
                ORDER BY cities.id ASC"""
     cursor.execute(query, (argv[4],))
